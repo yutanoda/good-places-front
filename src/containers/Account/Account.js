ecee7deff4b013bca0d566ace17ffabb30c41a68
yutanoda <atuyuta902@yahoo.co.jp>
2021-05-02 14:05:56 +0900

    icon 追加

--- src/containers/Account/Account.js
+++ src/containers/Account/Account.js
@@ -3,6 +3,10 @@ import Aux from '../../hoc/Aux/Aux';
 import { Link } from 'react-router-dom'
 import classes from './Account.css';
 import logo from '../../logo.svg';
+import { IconContext } from "react-icons";
+import { FaArrowDown, FaRegMap} from 'react-icons/fa';
+import { RiMapPin2Line } from "react-icons/ri";
+import { FiUsers } from "react-icons/fi";
 
 class Home extends Component {
   render () {
@@ -34,14 +38,20 @@ class Home extends Component {
           </section>
           <section className={classes.MenuContainer}>
             <ul className={classes.MenuBar}>
-              <li className={classes.MenuList}><Link to='/' className={classes.MenuLink}>Maps</Link></li>
-              <li className={classes.MenuList}><Link to='/' className={classes.MenuLink}>Spots</Link></li>
-              <li className={classes.MenuList}><Link to='/' className={classes.MenuLink}>Following</Link></li>
+              <li className={classes.MenuList}><Link to='/' className={classes.MenuLink}><span className={classes.MenuIcon}><FaRegMap /></span>Maps</Link></li>
+              <li className={classes.MenuList}><Link to='/' className={classes.MenuLink}><span className={classes.MenuIcon}><RiMapPin2Line /></span>Spots</Link></li>
+              <li className={classes.MenuList}><Link to='/' className={classes.MenuLink}><span className={classes.MenuIcon}><FiUsers /></span>Following</Link></li>
             </ul>
             <div className={classes.NewButtonContainer}>
-             <Link to='/' className={classes.NewButtonLink}><div className={classes.NewButton}><span>＋</span> POST</div></Link>
+             <Link to='/spot/new' className={classes.NewButtonLink}><div className={classes.NewButton}><span>＋</span> POST</div></Link>
             </div>
-            <div></div>
+            <IconContext.Provider value={{ color: "black", className: "global-class-name", size: "3em" }}>
+              <div className={classes.IconContainer}>
+                <FaArrowDown className={classes.Icon} />
+                <FaArrowDown className={classes.Icon} />
+                <FaArrowDown className={classes.Icon} />
+              </div>
+            </IconContext.Provider>
           </section>
           <section></section>
         </div>
