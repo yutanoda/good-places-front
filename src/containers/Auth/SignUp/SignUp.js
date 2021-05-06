49d9325174f8c793e1c08f05069ea85b9f8b3834
yutanoda <atuyuta902@yahoo.co.jp>
2021-05-07 08:16:16 +0900

    stateをセット

--- src/containers/Auth/SignUp/SignUp.js
+++ src/containers/Auth/SignUp/SignUp.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import classes from './SignUp.css'
 import {Link} from 'react-router-dom'
+import { connect } from 'react-redux';
+import * as actionTypes from '../../../store/actions';
 import { IoArrowForwardOutline} from "react-icons/io5";
 
 class SignUp extends Component {
+
+  state = {
+    email: null
+  }
+
+  inputChangedHandler = (event) => {
+    this.setState({ email: event.target.value })
+  }
+
   render () {
     return (
       <div className={classes.Container}>
         <div className={classes.SignUpContainer}>
           <h4 className={classes.Title}>SignUp</h4>
           <label>Email</label>
-          <div className={classes.FormContainer}><input className={classes.Form}></input></div>
+          <div className={classes.FormContainer}>
+            <input 
+              className={classes.Form} 
+              onChange={(event) => this.inputChangedHandler(event)}
+            />
+          </div>
           <div className={classes.ButtonContainer}>
             <button className={classes.Button}><span>Next</span><span><IoArrowForwardOutline /></span></button>
           </div>
@@ -22,5 +38,6 @@ class SignUp extends Component {
     );
   }
 }
+
 
 export default SignUp;
---
+++ src/store/reducers/signUp.js
@@ -0,0 +1,11 @@
+import * as actionTypes from '.actions';
+
+const initialState = {
+  email: null
+}
+
+const reducer = (state = initialState, action) => {
+
+};
+
+export default reducer;
